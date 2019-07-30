076b80a488a050960b2be035d69d500efc6ad166
Emily Lillie <emilylillie3@gmail.com>
2019-07-30 21:57:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,9 +7,9 @@ class Project
   end 
   
   def add_backer(backer)
-    @backer = backer 
-    @backers << backer 
-    @backer.back_project << self 
+    @backer = backer
+    @backers << @backer
+    backer.backed_projects << self
   end 
   
   
